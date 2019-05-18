73ff194e1bf03a15118dd2cb2adb2e6a62a295e4
DaHe <dannychavez141@users.noreply.github.com>
2019-05-17 19:53:56 -0500

    go pe

---
+++ plantas.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="zxx">
+<?php error_reporting(0);
+ include_once'./cabezera.php';  ?>
+
+
+<body>
+   
+    <!--/banner-bottom -->
+    <section class="banner-bottom py-5" id="contact">
+        <div class="container-fluid py-md-5 inner-sec-w3ls">
+          
+               <font color="red">
+   
+</font>
+                    <div class="form">
+                        <h3 class="text-capitalize mb-4 text-center">PLANTAS REGISTRADAS</h3>
+                        <form action="#" method="post">
+<h5>NOMBRE DE PLANTA <font color="red">*</font>:</h5>
+                            <div class="input-group mt-1">
+                               <input type="text" class="margin2 form-control" name="name" placeholder="Ingrese Nombre de la Planta" required="">
+                            
+                            </div>
+                            <div class="input-group mt-3">
+                                
+                                <input type="text" name="number" class="form-control" placeholder="phone number" required="">
+                            </div>
+                          
+                            <div class="input-group1 mt-3">
+                                <button class="submit btn form-control">Send </button>
+                            </div>
+                        </form>
+                    </div>
+
+            
+
+        </div>
+    </section>
+ 
+</body>
+
+</html>
---
+++ rplanta.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="zxx">
+<?php error_reporting(0);
+ include_once'./cabezera.php';  ?>
+
+
+<body>
+   
+    <!--/banner-bottom -->
+    <section class="banner-bottom py-5" id="contact">
+        <div class="container-fluid py-md-5 inner-sec-w3ls">
+          
+               
+                    <div class="form">
+                        <h3 class="text-capitalize mb-4 text-center">Get In touch</h3>
+                        <form action="#" method="post">
+                            <div class="input-group mt-3">
+                                <input type="text" class="margin2 form-control" name="name" placeholder="Full name" required="">
+                                <input type="text" name="name" class="form-control" placeholder="Subject" required="">
+                            </div>
+                            <div class="input-group mt-3">
+                                <input type="email" class="margin2 form-control" name="email" placeholder="mail@example.com" required="">
+                                <input type="text" name="number" class="form-control" placeholder="phone number" required="">
+                            </div>
+                            <div class="input-group mt-3">
+                                <textarea rows="4" cols="50" class="form-control" placeholder="message"></textarea>
+                            </div>
+                            <div class="input-group1 mt-3">
+                                <button class="submit btn form-control">Send </button>
+                            </div>
+                        </form>
+                    </div>
+
+            
+
+        </div>
+    </section>
+ 
+</body>
+
+</html>
