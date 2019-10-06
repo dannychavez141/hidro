eb4103208b21c2c0a6399778104396141084c1f8
danny chavez <dannychavez141@github.com>
2019-10-06 18:43:43 -0500

    Update subir.php

--- subir.php
+++ subir.php
@@ -13,7 +13,7 @@ $sql="INSERT INTO `hidroponia`.`reporte` (`hum`, `temp`, `hsuelo1`, `hsuelo2`, `
         $rs=$mysqli->query($sql);         
         $mysqli->close();
    header("Status: 301 Moved Permanently");
-header("Location: http://192.168.100.50");
+header("Location: http://192.168.100.5");
 exit;
 
 ?>
