4a03ff3fd2c9b12323df449c4f0106e5322e8723
danny chavez <dannychavez141@users.noreply.github.com>
2020-12-01 16:43:41 -0500

    Update hidro2.ino

--- hidro2/hidro2.ino
+++ hidro2/hidro2.ino
@@ -3,26 +3,20 @@
 #include "RTClib.h"
 #include <LiquidCrystal_I2C.h>
 #include "DHT.h"
-#define DHTPIN 30
+#define DHTPIN A0
 #define DHTTYPE DHT11
 #include <Keypad.h>
-String esllave1 = "OFF";
-String esllave2 = "OFF";
-String esllave3 = "OFF";
-int rep=0;
-LiquidCrystal_I2C lcd(0x27, 20, 4);
-const int llave1 = 38;
-const int llave2 = 39;
-const int llave3 = 40;
-const int bomba = 41;
-const int led1 = 3;
-const int led2 = 4;
-const int led3 = 5;
-const int led4 = 6;
+int rep = 0;
+LiquidCrystal_I2C lcd(0x3F, 20, 4);
+const int llave1 = 11;
+const int llave2 = 12;
+const int llave3 = 13;
+const int bomba = 10;
 DHT dht(DHTPIN, DHTTYPE);
-RTC_DS1307 RTC;
+RTC_DS3231 RTC;
 float contador = 0;
 int tiempo = 0;
+int duracion = 0;
 int hum1 = 0;
 int tem1 = 0;
 char temp2 = "";
@@ -41,54 +35,50 @@ const byte rowsCount = 4;
 const byte columsCount = 4;
 char key = "";
 char keys[rowsCount][columsCount] = {
-  { '1', '4', '7', '*' },
-  { '2', '5', '8', '0' },
-  { '3', '6', '9', '#' },
-  { 'A', 'B', 'C', 'D' }
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
 };
 char dato = 'A';
-const byte rowPins[rowsCount] = { 46, 47, 48, 49 };
-const byte columnPins[columsCount] = { 42, 43, 44, 45 };
-
+const byte rowPins[rowsCount] = { 9, 8, 7, 6 };
+const byte columnPins[columsCount] = { 5, 4, 3, 2 };
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, columnPins, rowsCount, columsCount);
 int h1 = 0, h2 = 0, m1 = 0, m2 = 0;
 int cambio = 1;
 char modo = 'A';
 char estado = 'a';
+int tmuestra = 1000;
+unsigned long tAhoraM = 0;
+unsigned long tRiego = 0;
 void setup() {
   lcd.init();
   lcd.backlight();
   Serial.begin(9600);
   RTC.begin();
   //RTC.adjust(DateTime(__DATE__, __TIME__)); // Establece la fecha y hora (Comentar una vez establecida la hora)
-
+  dht.begin();
   //actualizarbd(0, 6);
   //actualizarbd(1, 0);
   //actualizarbd(2, 18);
- // actualizarbd(3, 0);
- // actualizarbd(4, 30);
+  // actualizarbd(3, 0);
+  // actualizarbd(4, 30);
 
   h1 = leerbd(0);
   m1 = leerbd(1);
   h2 = leerbd(2);
   m2 = leerbd(3);
   tiempo = leerbd(4);
+  duracion = leerbd(5);
   pinMode(bomba, OUTPUT);
   pinMode(llave1, OUTPUT);
   pinMode(llave2, OUTPUT);
   pinMode(llave3, OUTPUT);
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
-  digitalWrite(llave1, HIGH);
-  digitalWrite(llave2, HIGH);
-  digitalWrite(llave3, HIGH);
+
+  digitalWrite(llave1, LOW);
+  digitalWrite(llave2, LOW);
+  digitalWrite(llave3, LOW);
   digitalWrite(bomba, HIGH);
-  digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);
-  digitalWrite(led3, LOW);
-  digitalWrite(led4, LOW);
 
 }
 
@@ -100,10 +90,10 @@ void eleccion(char dato) {
     temp = "0";
     modo = dato;
 
-  } else if ( dato == '#')
+  } else if ( dato == '*')
   { cambio = 1;
     temp = "0";
-  } else if ( dato == '*')
+  } else if ( dato == '#')
   { cambio = 2;
     temp = "0";
   }
@@ -134,41 +124,44 @@ void menu()
 }
 void modo1() {
   teclado();
-  valores();
-  if (c1 != 0) {
-    c1 = c1 + 1;
-    if (c1 > 100) {
-      cerrarllaves();
-     
+  if (millis() > tAhoraM + tmuestra) {
+    tAhoraM = millis();
+    valores();
+    if (c1 != 0) {
+      c1 = c1 + 1;
+      if (c1 > duracion) {
+        cerrarllaves();
+        c1=0;
+      }
     }
   }
+
 
   if (dato == '1') {
     cerrarllaves();
     delay(1000);
     abrirllave1();
-    
-    dato = "";
+    c1 = 1;
+    dato = ' ';
   }
   if (dato == '2') {
     cerrarllaves();
     delay(1000);
     abrirllave2();
-   
-    
-    dato = "";
+    c1 = 1;
+    dato = ' ';
   }
   if (dato == '3') {
     cerrarllaves();
 
     delay(1000);
     abrirllave3();
-    
-    dato = "";
+    c1 = 1;
+    dato = ' ';
   }
   if (dato == '4') {
     cerrarllaves();
-   
+ dato = ' ';
   }
   if (modo != 'A') {
     temp = "0";
@@ -177,7 +170,7 @@ void modo1() {
     h2 = leerbd(2);
     m2 = leerbd(3);
     tiempo = leerbd(4);
-
+    duracion = leerbd(5);
   }
   return;
 }
@@ -192,7 +185,7 @@ void modo2() {
     h2 = leerbd(2);
     m2 = leerbd(3);
     tiempo = leerbd(4);
-
+    duracion = leerbd(5);
   }
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -204,9 +197,12 @@ void modo2() {
   lcd.print(":");
   lcd.print(m1);
   lcd.print("/");
+
   if (modo == 'B') {
     if (cambio == 1)
-    { lcd.print("C.Hora");
+    {
+      lcd.setCursor(0, 2);
+      lcd.print("Cambiando Hora");
       if (temp == "0") {
         temp = h1;
       }
@@ -220,7 +216,8 @@ void modo2() {
 
 
     } else {
-      lcd.print("C.Minu");
+      lcd.setCursor(0, 2);
+      lcd.print("Cambiando Minutos");
       if (temp == "0") {
         temp = m1;
       }
@@ -251,7 +248,7 @@ void modo3() {
     h2 = leerbd(2);
     m2 = leerbd(3);
     tiempo = leerbd(4);
-
+    duracion = leerbd(5);
   }
 
   lcd.clear();
@@ -266,7 +263,8 @@ void modo3() {
   lcd.print(" / ");
   if (modo == 'C') {
     if (cambio == 1)
-    { lcd.print("C.Hora");
+    { lcd.setCursor(0, 2);
+      lcd.print("Cambiando Hora");
 
       if (temp == "0") {
         temp = h2;
@@ -279,7 +277,8 @@ void modo3() {
         temp = "0";
       }
     } else {
-      lcd.print("C.Minu");
+      lcd.setCursor(0, 2);
+      lcd.print("Cambiando Minutos");
       if (temp == "0") {
         temp = m2;
       }
@@ -303,30 +302,62 @@ void modo4() {
     Serial.print("entre aca:");
     Serial.println(temp);
     actualizarbd(4, tiempo);
+    actualizarbd(5, duracion);
     h1 = leerbd(0);
     m1 = leerbd(1);
     h2 = leerbd(2);
     m2 = leerbd(3);
     tiempo = leerbd(4);
+    duracion = leerbd(5);
   }
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("ENTRE TIEMPO");
+  if (cambio == 1)
+  {
+    lcd.print("ENTRE TIEMPO:*");
+  } else {
+    lcd.print("ENTRE TIEMPO:");
+  }
+
   lcd.setCursor(0, 1);
   lcd.print(tiempo);
-
   lcd.print(" Minu");
+  lcd.setCursor(0, 2);
+  if (cambio == 1)
+  {
+    lcd.print("DURACION:");
+  } else {
+    lcd.print("DURACION:*");
+  }
+  lcd.setCursor(0, 3);
+  lcd.print(duracion);
+  lcd.print(" Segu");
   if (modo == 'D') {
-    if (temp == "0") {
-      temp = tiempo;
+    if (cambio == 1)
+    {
+      if (temp == "0") {
+        temp = tiempo;
+      }
+      Serial.println(temp);
+      tiempo = temp.toInt();
+      if (tiempo > 240) {
+        tiempo = 0;
+        Serial.println("entre aca");
+        temp = "0";
+      }
+    } else {
+      if (temp == "0") {
+        temp = duracion;
+      }
+      Serial.println(temp);
+      duracion = temp.toInt();
+      if (duracion > 240) {
+        duracion = 0;
+        Serial.println("entre aca");
+        temp = "0";
+      }
     }
-    Serial.println(temp);
-    tiempo = temp.toInt();
-    if (tiempo > 180) {
-      tiempo = 0;
-      Serial.println("entre aca");
-      temp = "0";
-    }
+
   }
   delay(200);
 
@@ -338,13 +369,14 @@ void valores() {
   tem1 = dht.readTemperature();
   DateTime now = RTC.now();
   lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("|Hum:");
+ if (cambio == 1)
+    { lcd.setCursor(0, 0);
+  lcd.print("|Hum=");
   lcd.print(hum1);
   lcd.print("%|");
-  lcd.print("Tem:");
+  lcd.print("Tem=");
   lcd.print(tem1);
-  lcd.print("C|");
+  lcd.print("*C|");
   lcd.setCursor(0, 1);
   lcd.print(now.day(), DEC);
   lcd.print("/");
@@ -358,150 +390,136 @@ void valores() {
   lcd.print(":");
   lcd.print(now.second(), DEC);
   lcd.setCursor(0, 2);
-  lcd.print("PROx RIEGO:");
+  lcd.print("PROX RIEGO=");
   lcd.print(tiempo - contador);
   lcd.print("Min");
   lcd.setCursor(0, 3);
-  lcd.print("Ult riego:");
+  lcd.print("ULT.RIEGO=");
   lcd.print(horau);
   lcd.print(":");
   lcd.print(minuu);
   lcd.print(":");
   lcd.print(seguu);
-
+    }else{
+     lcd.setCursor(0, 0);
+  lcd.print("H.INICIO=");
+  lcd.print(h1);
+  lcd.print(":");
+  lcd.print(m1);
+  lcd.setCursor(0, 1);
+  lcd.print("H.FIN=");
+  lcd.print(h2);
+  lcd.print(":");
+  lcd.print(m2);
+  lcd.setCursor(0, 2);
+  lcd.print("RIEGO ENTRE=");
+  lcd.print(tiempo);
+  lcd.print("Min");
+  lcd.setCursor(0, 3);
+  lcd.print("DUR.RIEGO=");
+  lcd.print(duracion);
+  lcd.print("Seg");
+  
+      }
   hora = (now.hour());
   minu = (now.minute());
   segu = (now.second());
   if (segu == 0) {
-    if(rep==0){
-    contador = contador + 1;
-    rep=1;}
+    if (rep == 0) {
+      contador = contador + 1;
+      rep = 1;
+    }
   }
-   if (segu != 0) {
-    rep=0;
+  if (segu != 0) {
+    rep = 0;
   }
-  
-  if (hora > h1 && hora < h2) {
+  if (hora >= h1 && hora <= h2) {
     int evalua = tiempo - contador;
-    if (evalua < 0) {
+    if (evalua <= 0) {
       contador = 0;
+      Serial.println("AUTOMATICO");
+      Serial.println("LLAVE 1");
+      abrirllave1();
+      tRiego = millis();
+      estado = 'b';
     }
-    if (estado == 'a') {
+    Serial.print("Estado:");
+    Serial.println(estado);
+    Serial.print(millis());
+    Serial.print("/");
+    Serial.println(tRiego + (duracion * 1000));
 
-      if (evalua == 0 && segu == 0) {
-        cerrarllaves();
-        delay(1000);
-        abrirllave1();
-        c1 = 1;
-        estado = 'b';
-      }
-    }
     if (estado == 'b') {
-      if (evalua == 0 && segu == 10) {
+      if (millis() >= (tRiego + (duracion * 1000))) {
+        Serial.println("LLAVE 2");
         cerrarllaves();
         delay(1000);
-
         abrirllave2();
-        delay(1000);
-
-        c1 = 1;
+        tRiego = millis();
         estado = 'c';
       }
     }
     if (estado == 'c') {
-      if (evalua == 0 && segu == 21) {
-        delay(1000);
+      if (millis() >= (tRiego + (duracion * 1000))) {
+        Serial.println("LLAVE 3");
         cerrarllaves();
-
-
         delay(1000);
         abrirllave3();
-        c1 = 1;
+        tRiego = millis();
         estado = 'd';
       }
     }
     if (estado == 'd') {
-      if (evalua == -0 && segu == 32) {
-        delay(1000);
-        horau = hora;
-        minuu = minu;
-        seguu = segu;
+      if (millis() >= (tRiego + (duracion * 1000))) {
+        Serial.println("CERRADO");
         cerrarllaves();
-       
+        tRiego = millis();
         estado = 'a';
-        contador = 0;
       }
-
     }
   }
 
+
 }
 void abrirllave1() {
-  digitalWrite(llave1, LOW);
-  digitalWrite(llave2, HIGH);
-  digitalWrite(llave3, HIGH);
-  digitalWrite(bomba, LOW);
-  digitalWrite(led1, HIGH);
-  digitalWrite(led2, LOW);
-  digitalWrite(led3, LOW);
-  digitalWrite(led4, HIGH);
-  esllave1 = "ON";
-  esllave2 = "OFF";
-  esllave3 = "OFF";
-  horau = hora;
-    minuu = minu;
-    seguu = segu;
-  c1 = 1;
-};
-void abrirllave2() {
- 
+  Serial.println("abierto 1");
   digitalWrite(llave1, HIGH);
   digitalWrite(llave2, LOW);
-  digitalWrite(llave3, HIGH);
+  digitalWrite(llave3, LOW);
   digitalWrite(bomba, LOW);
-  digitalWrite(led1, LOW);
-  digitalWrite(led2, HIGH);
-  digitalWrite(led3, LOW);
-  digitalWrite(led4, HIGH);
   horau = hora;
-    minuu = minu;
-    seguu = segu;
-  esllave1 = "OFF";
-  esllave2 = "ON";
-  esllave3 = "OFF";
-  c1 = 1;
+  minuu = minu;
+  seguu = segu;
 };
-void abrirllave3() {
-  digitalWrite(llave1, HIGH);
+void abrirllave2() {
+Serial.println("abierto 2");
+  digitalWrite(llave1, LOW);
   digitalWrite(llave2, HIGH);
   digitalWrite(llave3, LOW);
   digitalWrite(bomba, LOW);
-  digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);
-  digitalWrite(led3, HIGH);
-  digitalWrite(led4, HIGH);
-  esllave1 = "OFF";
-  esllave2 = "OFF";
-  esllave3 = "ON";
   horau = hora;
-    minuu = minu;
-    seguu = segu;
-  c1 = 1;
+  minuu = minu;
+  seguu = segu;
+
+};
+void abrirllave3() {
+  Serial.println("abierto 3");
+  digitalWrite(llave1, LOW);
+  digitalWrite(llave2, LOW);
+  digitalWrite(llave3, HIGH);
+  digitalWrite(bomba, LOW);
+  horau = hora;
+  minuu = minu;
+  seguu = segu;
 
 };
 void cerrarllaves() {
-  digitalWrite(llave1, HIGH);
-  digitalWrite(llave2, HIGH);
-  digitalWrite(llave3, HIGH);
+  Serial.println("cerrado");
+  digitalWrite(llave1, LOW);
+  digitalWrite(llave2, LOW);
+  digitalWrite(llave3, LOW);
   digitalWrite(bomba, HIGH);
-  digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);
-  digitalWrite(led3, LOW);
-  digitalWrite(led4, LOW);
-  esllave1 = "OFF";
-  esllave2 = "OFF";
-  esllave3 = "OFF";
-  c1 = 0;
+
 };
 
 void teclado() {
